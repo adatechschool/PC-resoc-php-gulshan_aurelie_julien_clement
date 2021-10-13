b07eb30bc1e7b012f8b42a75ef88fb75bdbb1c59
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-10-13 21:18:46 +0200

    liste des mots-cles
    et messages correspondants

--- src/tags.php
+++ src/tags.php
@@ -12,104 +12,58 @@ session_start();
     <body>
         <header>
             <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-
             <?php
-            $mysqli = new mysqli("localhost:3306", "root", "root", "socialnetwork");
-            $mysqli->set_charset("utf8mb4");
+                $mysqli = new mysqli("localhost:3306", "root", "root", "socialnetwork");
+                $mysqli->set_charset("utf8mb4");
 
-            $userEnSql = "SELECT users.id, posts_tags.tag_id FROM `users`"
-            ." INNER JOIN `posts` ON posts.user_id = users.id"
-            ." INNER JOIN `posts_tags` ON posts_tags.post_id = posts.id";
-
-            $userInfo = $mysqli->query($userEnSql);
-
-            if ($link = $userInfo->fetch_assoc())
-            {   
-                $tagId = $link['tag_id'];
-                $userId = $link['id'];
-            } ?>
-
-            <?php
                 include("menu.php");
                 print_menu(isset($_SESSION['connected_id']) ? $_SESSION['connected_id'] : 0);
-            ?> 
+
+        //verification de la connexion
+                if ($mysqli->connect_errno)
+                {
+                    echo("Échec de la connexion : " . $mysqli->connect_error);
+                    exit();
+                }
+            ?>
 
         </header>
         <div id="wrapper">
             <aside>
-                <?php
-                /**
-                 * Etape 3: récupérer le nom du mot-clé
-                 */
-                $laQuestionEnSql = "SELECT * FROM `tags` WHERE id=" . intval($tagId);
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                $tag = $lesInformations->fetch_assoc();
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par le label et effacer la ligne ci-dessous
-                ?>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez les derniers messages comportant
-                        le mot-clé <?php echo $tag['label'] ?>
-                        (n° <?php echo $link['tag_id'] ?>)
+                    <p>Sur cette page vous trouverez tous les mots-clés identifiés sur ce
+                        reseau social : 
                     </p>
-
                 </section>
             </aside>
+
             <main>
                 <?php
                 /**
-                 * Etape 3: récupérer tous les messages avec un mot clé donné
+                 * Récupérer tous les mots-clés
                  */
-                $laQuestionEnSql = "SELECT `posts`.`content`,"
-                        . "`posts`.`created`,"
-                        . "`users`.`alias` as author_name,  "
-                        . "`users`.`id` as user_id,  "
-                        . "count(`likes`.`id`) as like_number,  "
-                        . "GROUP_CONCAT(DISTINCT `tags`.`label`) AS taglist "
-                        . "FROM `posts_tags` as filter "
-                        . "JOIN `posts` ON `posts`.`id`=filter.`post_id`"
-                        . "JOIN `users` ON `users`.`id`=`posts`.`user_id`"
-                        . "LEFT JOIN `posts_tags` ON `posts`.`id` = `posts_tags`.`post_id`  "
-                        . "LEFT JOIN `tags`       ON `posts_tags`.`tag_id`  = `tags`.`id` "
-                        . "LEFT JOIN `likes`      ON `likes`.`post_id`  = `posts`.`id` "
-                        . "WHERE filter.`tag_id`='" . intval($tagId) . "' "
-                        . "GROUP BY `posts`.`id`"
-                        . "ORDER BY `posts`.`created` DESC  "
-                ;
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
-                {
-                    echo("Échec de la requete : " . $mysqli->error);
-                }
+                    $laQuestionEnSql = "SELECT * FROM `tags`";
+                    $lesInformations = $mysqli->query($laQuestionEnSql);
+                // Vérification
+                    if ( ! $lesInformations)
+                    {
+                        echo("Échec de la requete : " . $mysqli->error);
+                        exit();
+                    }
 
-                /**
-                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
-                 */
-                while ($post = $lesInformations->fetch_assoc())
-                {
-                    ?>                
+                    while ($tag = $lesInformations->fetch_assoc())
+                    {
+                    ?>
                     <article>
-                        <h3>
-                            <time datetime='<?php echo $post['created'] ?>' ><?php echo $post['created'] ?></time>
-                        </h3>
-                        <address>par 
-                        <a href="wall.php?user_id=<?php echo $post['user_id'] ?>"><?php echo $post['author_name'] ?></a>
-                    </address>
-                    <div>
-                        <p><?php echo $post['content'] ?></p>
-                    </div>                                            
-                    <footer>
-                        <small>♥ <?php echo $post['like_number'] ?></small>
-                        <?php $posts = explode (",", $post['taglist']);
-                                foreach ($posts as $singlevalue) {
-                                echo "<a href=''>".$singlevalue."</a> &nbsp;";
-                                }
-                            ?>                                         
-                        </footer>
+                        <h3><?php echo $tag['label'] ?></h3>
+                        <p>id:<?php echo $tag['id'] ?></p>
+                        <nav>
+                            <!-- a href="tags.php?tag_id=< ?php echo $tag['id']?>">Messages</a -->
+                            <a href="tags_byTagId.php?tag_id=<?php echo $tag['id']?>">Messages</a>
+                        </nav>
                     </article>
                 <?php } ?>
-
 
             </main>
         </div>
---
+++ src/tags_byTagId.php
@@ -0,0 +1,114 @@
+<?php
+session_start(); 
+?>
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>ReSoC - Les message par mot-clé</title> 
+        <meta name="author" content="Julien Falconnet">
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <header>
+            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
+
+            <?php
+            $mysqli = new mysqli("localhost:3306", "root", "root", "socialnetwork");
+            $mysqli->set_charset("utf8mb4");
+            
+        //verification de la connexion
+            if ($mysqli->connect_errno)
+            {
+                echo("Échec de la connexion : " . $mysqli->connect_error);
+                exit();
+            }
+
+            include("menu.php");
+            print_menu(isset($_SESSION['connected_id']) ? $_SESSION['connected_id'] : 0);
+            ?> 
+
+        </header>
+        <div id="wrapper">
+            <?php
+            $tagId = $_GET['tag_id'];
+            ?>
+
+            <aside>
+
+                <?php
+                /**
+                 * Récupérer le mot-clé
+                 */
+                $laQuestionEnSql = "SELECT * FROM `tags` WHERE id=" . intval($tagId);
+                $lesInformations = $mysqli->query($laQuestionEnSql);
+                $tag = $lesInformations->fetch_assoc();
+                ?>
+
+                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+                <section>
+                    <h3>Présentation</h3>
+                    <p>Sur cette page vous trouverez les derniers messages comportant
+                        le mot-clé
+                        (n° <?php echo $_GET['tag_id'] ?>)
+                    </p>
+                </section>
+            </aside>
+
+            <main>
+                <?php
+                /**
+                 * Récupérer tous les messages avec un mot clé donné
+                 */
+                $laQuestionEnSql = "SELECT `posts`.`content`,"
+                        . "`posts`.`created`,"
+                        . "`users`.`alias` as author_name,  "
+                        . "count(`likes`.`id`) as like_number,  "
+                        . "GROUP_CONCAT(DISTINCT `tags`.`label`) AS taglist "
+                        . "FROM `posts_tags` as filter "
+                        . "JOIN `posts` ON `posts`.`id`=filter.`post_id`"
+                        . "JOIN `users` ON `users`.`id`=`posts`.`user_id`"
+                        . "LEFT JOIN `posts_tags` ON `posts`.`id` = `posts_tags`.`post_id`  "
+                        . "LEFT JOIN `tags`       ON `posts_tags`.`tag_id`  = `tags`.`id` "
+                        . "LEFT JOIN `likes`      ON `likes`.`post_id`  = `posts`.`id` "
+                        . "WHERE filter.`tag_id`='" . intval($tagId) . "' "
+                        . "GROUP BY `posts`.`id`"
+                        . "ORDER BY `posts`.`created` DESC  "
+                ;
+                $lesInformations = $mysqli->query($laQuestionEnSql);
+                if ( ! $lesInformations)
+                {
+                    echo("Échec de la requete : " . $mysqli->error);
+                }
+
+                /**
+                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+                 */
+                while ($post = $lesInformations->fetch_assoc())
+                {
+                ?>                
+                    <article>
+                        <h3>
+                            <time datetime='<?php echo $post['created'] ?>' ><?php echo $post['created'] ?></time>
+                        </h3>
+                        <address>par 
+                        <a href="wall.php?user_id=<?php echo $post['user_id'] ?>"><?php echo $post['author_name'] ?></a>
+                        </address>
+                    <div>
+                        <p><?php echo $post['content'] ?></p>
+                    </div>                                            
+                    <footer>
+                        <small>♥ <?php echo $post['like_number'] ?></small>
+                        <?php $posts = explode (",", $post['taglist']);
+                                foreach ($posts as $singlevalue) {
+                                echo "<a href=''>".$singlevalue."</a> &nbsp;";
+                                }
+                        ?>                                         
+                    </footer>
+                    </article>
+                <?php } ?>
+
+            </main>
+        </div>
+    </body>
+</html>
